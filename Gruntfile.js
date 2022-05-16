e4826127788d4f5369d1e64fc148f92359b0bc58
Amata Eder <amata.eder@students.fh-hagenberg.at>
2022-05-16 14:21:55 +0200

    Did some grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        htmlmin: {                                     // Task
+            dist: {                                      // Target
+                options: {                                 // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    'index.min.html': 'index.html',     // 'destination': 'source'
+                }
+            }
+        },
+        htmllint: {
+            all: ['*.html']
+        },
+        jsonlint: {
+            sample: {
+                src: [ 'json/appointments.json' ],
+                options: {
+                    formatter: 'prose'
+                }
+            }
+        },
+        concat: {
+            dist: {
+                src: ['assets/js/admin.js', 'assets/js/appointment.js', 'assets/js/dashboard.js', 'assets/js/person.js', 'assets/js/main.js'],
+                dest: 'public/js/main.js',
+            },
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'public/js/main.min.js': ['public/js/main.js']
+                }
+            }
+        },
+        cssmin: {
+          target: {
+              files: {
+                  'public/css/styles.min.css': ['public/css/styles.css']
+              }
+          }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass'],
+                options: {
+                    livereload: true,
+                },
+            }
+        },
+        sass: {                              // Task
+            dist: {                            // Target
+                options: {                       // Target options
+                    style: 'expanded'
+                },
+                files: {                         // Dictionary of files
+                    'public/css/styles.css': 'assets/scss/styles.scss',       // 'destination': 'source'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.loadNpmTasks('grunt-jsonlint');
+    grunt.loadNpmTasks('grunt-html');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+    grunt.registerTask('default', ['sass', 'cssmin', 'concat', 'uglify']);
+};
+
